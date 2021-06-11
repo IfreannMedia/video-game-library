e3e350abce4f92a67d53a667f82fb1f48d35da0b
Wario <warrenlinkedin438@gmail.com>
2021-06-11 21:11:53 +0200

    unsubbed from subs

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -2,14 +2,14 @@ import { HttpService } from './../../services/http.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from './../../models';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
 
   public gameRating = 0;
   public gameId: string;
@@ -25,7 +25,11 @@ export class DetailsComponent implements OnInit {
       this.gameId = params['id'];
       this.getGameDetails(this.gameId);
     });
+  }
 
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+    this.gameSub.unsubscribe();
   }
 
   getGameDetails(id: string): void {
